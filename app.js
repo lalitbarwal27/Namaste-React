446ba946512f8a34688c4d257f67cf0c240dc456
lalitbarwal27 <lalitbarwal27@gmail.com>
2024-08-23 23:04:13 +0530

    Building Restaurant

--- app.js
+++ app.js
@@ -8,20 +8,22 @@
 
 import React from "react";
 import ReactDOM from 'react-dom'
+import restaurantData from './resturant.json'
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [ React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "this is a h1 tag")
-  ), React.createElement(
-    "div",
-    { id: "child2" },
-    React.createElement("h1", {}, "this is a h2 tag")
-  )]
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//  [ React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "this is a h1 tag")
+//   ), React.createElement(
+//     "div",
+//     { id: "child2" },
+//     React.createElement("h1", {}, "this is a h2 tag")
+//   )]
+// );
+
 
 console.log(parent)
 
@@ -35,6 +37,69 @@ const Title =() => (
 
 //fucntional component 
 
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" alt="logo"/>
+      </div>
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>News</li>
+          <li>Cart</li> 
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const Search = () => {
+  <div>Search</div>
+}
+
+const RestaurantCard = (props) => {
+  
+
+  const {info}=props.restData
+  const {name,costForTwo,cuisines,avgRating,sla,cloudinaryImageId}=info
+
+  return (
+  <div key={info.id} className="cardContainer">
+    <img className="restImage" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt='' />
+    <h4>{name}</h4>
+    <h4>{cuisines.join(' ,')}</h4>
+    <h4>{costForTwo}</h4>
+    <h4>{avgRating}</h4>
+    <h4>{sla.deliveryTime}</h4>
+  </div>
+  )
+}
+
+
+
+const Body = () => {
+  return (
+    <div>
+      <Search/>
+      <div className="container">
+      {restaurantData.restaurants.map((rest) => <RestaurantCard key={rest.info.id} restData={rest}/> )}
+      
+</div>
+    </div>
+  )
+}
+
+const App =() => {
+  return (
+  <div className="mainContainer">
+    <Header/>
+    <Body/>
+  </div>)
+}
+
 const HeadingComponent = () =>(
   <div id="container">
     <Title>
@@ -45,4 +110,4 @@ const HeadingComponent = () =>(
   </div>
 )
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent/>)
+root.render(<App/>)
